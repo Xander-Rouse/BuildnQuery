095dbb9c4d6a679ea9aabdf39531058a814df94b
Xander-Rouse <80725102+Xander-Rouse@users.noreply.github.com>
2021-05-10 14:06:44 +1000

    “Task 1 Complete”

--- Build.sql
+++ Build.sql
@@ -1,1 +1,7 @@
-
+/*TOUR (TourName, Description)
+Primary Key (TourName)
+EVENT (EventYear, EventMonth, EventDay, Fee)
+Primary Key (EventYear, EventMonth, EventDay)
+BOOKING (DateBooked, Payment)
+CLIENT (ClientID, Surname, GivenName, Gender)
+Primary Key (ClientID)*/
